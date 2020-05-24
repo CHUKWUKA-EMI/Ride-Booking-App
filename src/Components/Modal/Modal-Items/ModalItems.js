ecc7189415d8e9a3dcf2ba91a2b69c98201f83ae
Chukwuka Emi <emichukwuka@gmail.com>
2020-05-24 13:31:52 +0100

    worked on the trips page

--- src/Components/Modal/Modal-Items/ModalItems.js
+++ src/Components/Modal/Modal-Items/ModalItems.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./ModalItems.css";
 
-const ModalItems = () => {
+const ModalItems = (props) => {
 	return (
 		<div className="modal-Items">
-			<div>
+			<span className="item-features">
+				<label style={{ marginLeft: "3%" }}>{props.direction}</label>
+				<label>{props.duration}</label>
+				<label>{props.vehicle}</label>
+				<label>{props.cost}</label>
+			</span>
+
+			<div style={{ width: "100px", marginLeft: "75%" }}>
 				<button
 					className="btn"
-					style={{ marginTop: "40PX", marginLeft: "83%" }}>
+					style={{ marginTop: "20px", marginLeft: "83%" }}>
 					Book
 				</button>
 			</div>
--- src/Components/Modal/modal.js
+++ src/Components/Modal/modal.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./modal.css";
 import ModalItems from "./Modal-Items/ModalItems";
 
 const Modal = () => {
+	const [routes, setRoutes] = useState([]);
+
+	const fetchRoutes = () => {
+		let requestBody = {
+			query: `
+         query{
+            routes{
+              id
+              direction
+              vehicle
+              duration
+              cost
+            }
+       }
+      `,
+		};
+
+		fetch("https://ride-booking-app.herokuapp.com/graphql", {
+			method: "post",
+			body: JSON.stringify(requestBody),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => {
+				if (res.status !== 200 && res.status !== 201) {
+					throw new Error("Authentication Failed");
+				}
+				return res.json();
+			})
+			.then((resData) => {
+				//console.log(resData.data.routes);
+				const updatedRoutes = resData.data.routes;
+				setRoutes(updatedRoutes);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	useEffect(() => {
+		fetchRoutes();
+	}, []);
+
 	return (
 		<div className="modal-class">
 			<p>Available Routes</p>
@@ -12,11 +55,36 @@ const Modal = () => {
 				<label>Vehicle</label>
 				<label>Cost</label>
 			</span>
-			<ModalItems />
-			<ModalItems />
-			<ModalItems />
-			<ModalItems />
-			<ModalItems />
+			<ModalItems
+				direction="Lagos - kano"
+				duration="10 hours"
+				vehicle="Bus"
+				cost="$100"
+			/>
+			<ModalItems
+				direction="Lagos - kano"
+				duration="10 hours"
+				vehicle="Bus"
+				cost="$100"
+			/>
+			<ModalItems
+				direction="Lagos - kano"
+				duration="10 hours"
+				vehicle="Bus"
+				cost="$100"
+			/>
+			<ModalItems
+				direction="Lagos - kano"
+				duration="10 hours"
+				vehicle="Bus"
+				cost="$100"
+			/>
+			<ModalItems
+				direction="Lagos - kano"
+				duration="10 hours"
+				vehicle="Bus"
+				cost="$100"
+			/>
 		</div>
 	);
 };
